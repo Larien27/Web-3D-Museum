e8d034c72798d18adb6c23333e1f5a500af296b6
Larien27 <lucie.beke@gmail.com>
2024-10-27 15:47:42 +0100

    Added floor and walls of the museum room

--- frontend/src/script.js
+++ frontend/src/script.js
@@ -13,7 +13,51 @@ const canvas = document.querySelector('canvas.webgl');
 // Scene
 const scene = new THREE.Scene();
 
-// Object
+/**
+ * Museum
+ */
+const roomDimensions = {
+    width: 12,
+    length: 15,
+    height: 5
+}
+
+// Floor
+const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(roomDimensions.width, roomDimensions.length, 1),
+    new THREE.MeshStandardMaterial()
+);
+floor.rotation.x = - Math.PI / 2;
+scene.add(floor);
+
+// Walls
+const longerWallGeometry = new THREE.PlaneGeometry(roomDimensions.length, roomDimensions.height);
+const shorterWallGeometry = new THREE.PlaneGeometry(roomDimensions.width, roomDimensions.height);
+const wallMaterial = new THREE.MeshStandardMaterial();
+
+const longWall1 = new THREE.Mesh(longerWallGeometry, wallMaterial);
+const longWall2 = new THREE.Mesh(longerWallGeometry, wallMaterial);
+const shortWall1 = new THREE.Mesh(shorterWallGeometry, wallMaterial);
+const shortWall2 = new THREE.Mesh(shorterWallGeometry, wallMaterial);
+
+longWall1.rotation.y = Math.PI / 2;
+longWall1.position.x = - roomDimensions.width / 2;
+longWall1.position.y = roomDimensions.height / 2;
+
+longWall2.rotation.y = - Math.PI / 2;
+longWall2.position.x = roomDimensions.width / 2;
+longWall2.position.y = roomDimensions.height / 2;
+
+shortWall1.position.z = - roomDimensions.length / 2;
+shortWall1.position.y = roomDimensions.height / 2;
+
+shortWall2.rotation.y = Math.PI;
+shortWall2.position.z = roomDimensions.length / 2;
+shortWall2.position.y = roomDimensions.height / 2;
+
+scene.add(longWall1, longWall2, shortWall1, shortWall2);
+
+// Example Object - TODO: DELETE LATER
 const cube = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1),
     new THREE.MeshStandardMaterial()
@@ -29,10 +73,10 @@ cubeDebug
     .add(cube.material, 'wireframe');
 
 // Lights
-const ambientLight = new THREE.AmbientLight('#ffffff', 0.8);
+const ambientLight = new THREE.AmbientLight('#ffffff', 1);
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight('#0000ff', 5);
+const directionalLight = new THREE.DirectionalLight('#0000ff', 2);
 directionalLight.position.x = 3;
 directionalLight.position.y = 2;
 directionalLight.position.z = 1;
