384b203b6d51a6d4c1eb332d28f613cd7d9c75f3
Larien27 <lucie.beke@gmail.com>
2024-11-06 20:08:51 +0100

    Added leva GUI

--- frontend/src/components/Lights.jsx
+++ frontend/src/components/Lights.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { useControls } from 'leva';
 
 function Lights() {
+    const { ambientIntensity, ambientColor } = useControls('Ambient Light', {
+        ambientIntensity: { value: 1, min: 0, max: 5 },
+        ambientColor: '#ffffff'
+    }, { collapsed: true });
+    
+    const { directionalIntensity, directionalColor } = useControls('Directional Light', {
+        directionalIntensity: { value: 2, min: 0, max: 5 },
+        directionalColor: '#ffffff'
+    }, { collapsed: true });
+
     return(
         <>
-        <ambientLight intensity={1}/>
-        <directionalLight position={[3, 2, 1]} intensity={2}/>
+            <ambientLight intensity={ambientIntensity} color={ambientColor} />
+            <directionalLight position={[3, 2, 1]} intensity={directionalIntensity} color={directionalColor} />
         </>
     );
 }
