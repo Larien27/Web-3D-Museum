d09250254728fe6609b04e65e9c0e86fc318f8bb
Larien27 <lucie.beke@gmail.com>
2024-11-09 22:38:55 +0100

    Toggle outline of the test cube with click

--- frontend/src/components/TestCube.jsx
+++ frontend/src/components/TestCube.jsx
@@ -1,8 +1,20 @@
+import { useState } from 'react';
+import { Outlines } from '@react-three/drei';
+
 function TestCube() {
+    const [clicked, setClicked] = useState(false);
+
     return(
-        <mesh position-y={1}>
+        <mesh
+            position-y={1}
+            onClick={() => setClicked(!clicked)}
+        >
             <boxGeometry args={[1, 1, 1]} />
             <meshStandardMaterial color={'red'} />
+
+            {clicked && (
+                <Outlines thickness={5} color={'white'} />
+            )}
         </mesh>
     );
 }
