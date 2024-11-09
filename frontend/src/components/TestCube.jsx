220e2e1aaa77c066aaca74f2122491f467ea3160
Larien27 <lucie.beke@gmail.com>
2024-11-09 22:56:18 +0100

    Added comments

--- frontend/src/components/TestCube.jsx
+++ frontend/src/components/TestCube.jsx
@@ -7,11 +7,13 @@ function TestCube() {
     return(
         <mesh
             position-y={1}
-            onClick={() => setClicked(!clicked)}
+            onClick={() => setClicked(!clicked)}        // Set state on click to toggle outline
+            onPointerDown={(e) => e.stopPropagation()}  // Stop controlls problems when the outline is on
         >
             <boxGeometry args={[1, 1, 1]} />
             <meshStandardMaterial color={'red'} />
 
+            {/* Set outline on click */}
             {clicked && (
                 <Outlines thickness={5} color={'white'} />
             )}
