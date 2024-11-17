6f9fad651cc2d52663a609914ab58f088668462a
Larien27 <lucie.beke@gmail.com>
2024-11-17 14:14:15 +0100

    Added new components to main page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,18 @@ import Header from './components/Header';
 import Login from './pages/login/Login';
 import Registration from './pages/registration/Registration';
 import Exhibition from './pages/exhibition/Exhibition';
+import ExhibitionList from './pages/exhibition-list/ExhibitionList';
+import Settings from './pages/settings/Settings';
 
 function App() {
     return(
         <>
             <Header />
-            {/* <Exhibition /> */}
+            <Exhibition />
             <Registration />
             <Login />
+            <ExhibitionList />
+            <Settings />
         </>
     );
 }
