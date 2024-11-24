dbffa8faef20f754843df6564efe241406c76fe2
Larien27 <lucie.beke@gmail.com>
2024-11-24 12:12:51 +0100

    Added all pages to the header menu

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -10,10 +10,14 @@ function Header() {
                 </div>
 
                 <ul>
+                    <li><NavLink to='/exhibition'>Exhibition</NavLink></li>
                     <li><NavLink to='/login'>Login</NavLink></li>
                     <li><NavLink to='/registration'>Registration</NavLink></li>
                     <li><NavLink to='/favourites'>Favourites</NavLink></li>
                     <li><NavLink to='/settings'>Settings</NavLink></li>
+                    <li><NavLink to='/exhibition-list'>Exhibition List</NavLink></li>
+                    <li><NavLink to='/users-table'>Users Table</NavLink></li>
+                    <li><NavLink to='/reports-list'>Reports List</NavLink></li>
                 </ul>
             </nav>
         </header>
