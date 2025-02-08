5ef8e26ea4ded6faaa94321a1ef4cb90a15dfd65
Larien27 <lucie.beke@gmail.com>
2025-02-08 10:57:46 +0100

    Deleted redundant file and piece of code

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -6,8 +6,5 @@ const router = express.Router();
 // Define user routes
 router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
-router.get('/exhibition-list',authMiddleware, (req, res) => {
-    res.json({ message: 'Protected data' });
-});
 
 module.exports = router;
