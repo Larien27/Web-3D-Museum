2754a81a037be21c8ef23c5148da22e77ec862b8
Larien27 <lucie.beke@gmail.com>
2025-03-15 17:09:01 +0100

    [FEAT] Artefact detail link from exhibition detail

--- frontend/src/pages/exhibition-detail/ExhibitionDetail.jsx
+++ frontend/src/pages/exhibition-detail/ExhibitionDetail.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 function ExhibitionDetail() {
     const { exhibitionId } = useParams();
     const [exhibition, setExhibition] = useState(null);
+    const [artefacts, setArtefacts] = useState([]);
     const [error, setError] = useState(null);
 
     useEffect(() => {
@@ -17,7 +19,18 @@ function ExhibitionDetail() {
             }
         }
 
+        async function fetchArtefacts() {
+            try {
+                const response = await axios.get(`/api/artefacts/exhibition/${exhibitionId}`);
+                setArtefacts(response.data);
+            } catch (err) {
+                setError('Failed to load exhibition.');
+            }
+        }
+
         fetchExhibition();
+        fetchArtefacts();
+
     }, [exhibitionId]);
 
     if (error) return <p className='error'>{error}</p>;
@@ -29,9 +42,17 @@ function ExhibitionDetail() {
             <img>{exhibition.imageUrl}</img>
             <p>{exhibition.description}</p>
 
-            {/* {exhibition.artefacts.map((artefact) =>(
-                <li>{artefact.title}</li>
-            ))} */}
+            {artefacts.length > 0 ? (
+                <ul>
+                    {artefacts.map((artefact) => (
+                        <li key={artefact.id}>
+                            <Link to={`/artefacts/${artefact.id}`}>{artefact.title}</Link>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>No artefacts found for this exhibition.</p>
+            )}
         </div>
     );
 }
