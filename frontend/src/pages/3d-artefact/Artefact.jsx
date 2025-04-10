82625ea6f63ee5801e14a6661f64f4f28a0d9041
Larien27 <lucie.beke@gmail.com>
2025-04-10 17:37:37 +0200

    [FIX] 3D Artefact lighting intensity change

--- frontend/src/pages/3d-artefact/Artefact.jsx
+++ frontend/src/pages/3d-artefact/Artefact.jsx
@@ -11,7 +11,7 @@ function Artefact({ artefactUrl }) {
             }}
         >        
             <ambientLight intensity={0.5} />
-            <directionalLight position={[5, 5, 5]} intensity={1} />
+            <directionalLight position={[5, 5, 5]} intensity={0.8} />
             <pointLight position={[0, 5, 0]} intensity={1} />
 
             <SimpleModel url={artefactUrl} />
