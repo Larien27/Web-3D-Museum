4797534158e4274e0043fada67a7d70b8db6d55b
Larien27 <lucie.beke@gmail.com>
2025-04-23 12:12:47 +0200

    [FIX] Removed 3D scene light setting panel

--- frontend/src/pages/3d-exhibition/Lights.jsx
+++ frontend/src/pages/3d-exhibition/Lights.jsx
@@ -1,20 +1,8 @@
-import { useControls } from 'leva';
-
 function Lights() {
-    const { ambientIntensity, ambientColor } = useControls('Ambient Light', {
-        ambientIntensity: { value: 1, min: 0, max: 5 },
-        ambientColor: '#ffffff'
-    }, { collapsed: true });
-    
-    const { directionalIntensity, directionalColor } = useControls('Directional Light', {
-        directionalIntensity: { value: 2, min: 0, max: 5 },
-        directionalColor: '#ffffff'
-    }, { collapsed: true });
-
     return(
         <>
-            <ambientLight intensity={ambientIntensity} color={ambientColor} />
-            <directionalLight position={[3, 2, 1]} intensity={directionalIntensity} color={directionalColor} />
+            <ambientLight intensity={1} color='#ffffff' />
+            <directionalLight position={[3, 2, 1]} intensity={2} color='#ffffff' />
         </>
     );
 }
