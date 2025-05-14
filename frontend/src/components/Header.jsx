825f1daf4d31da7dad63f67959381100f5790989
Larien27 <lucie.beke@gmail.com>
2025-05-14 10:48:48 +0200

    [FIX] Reponsive Header CSS

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,35 +1,43 @@
 import { NavLink } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import AuthContext from '../context/AuthContext';
 import './Header.scss';
 
 function Header() {
     const { user, logout } = useContext(AuthContext);
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => setMenuOpen(!menuOpen);
+    const closeMenu = () => setMenuOpen(false);
 
     return(
         <header id='header'>
             <nav>
                 <div id='logo'>3D Web Museum</div>
 
-                <ul>
+                <button className='hamburger' onClick={toggleMenu}>
+                    ☰
+                </button>
+
+                <ul className={menuOpen ? 'open' : ''}>
                     {user ? (
                         <>
-                            <li className='colorful-button'><NavLink to='/exhibition-list'>Exhibitions</NavLink></li>
+                            <li className={menuOpen ? '' : 'colorful-button'}><NavLink to='/exhibition-list' onClick={closeMenu}>Exhibitions</NavLink></li>
                             
                             {user.role === 'Admin' && (
                                 <>
-                                    <li><NavLink to='/users-table'>Users</NavLink></li>
-                                    <li><NavLink to='/reports-list'>Reports</NavLink></li>
+                                    <li><NavLink to='/users-table' onClick={closeMenu}>Users</NavLink></li>
+                                    <li><NavLink to='/reports-list' onClick={closeMenu}>Reports</NavLink></li>
                                 </>
                             )}
                             
-                            <li><NavLink to='/settings'>Settings</NavLink></li>
-                            <li><button className='nav-link-button' onClick={logout}>Log Out</button></li>
+                            <li><NavLink to='/settings' onClick={closeMenu}>Settings</NavLink></li>
+                            <li><button className='nav-link-button' onClick={() => { logout(); closeMenu(); }}>Log Out</button></li>
                         </>
                     ) : (
                         <>
-                            <li><NavLink to='/login'>Log In</NavLink></li>
-                            <li className='colorful-button'><NavLink to='/registration'>Sign Up</NavLink></li>
+                            <li><NavLink to='/login' onClick={closeMenu}>Log In</NavLink></li>
+                            <li className={menuOpen ? '' : 'colorful-button'}><NavLink to='/registration' onClick={closeMenu}>Sign Up</NavLink></li>
                         </>
                     )}
                 </ul>
